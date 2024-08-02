845d54f2e8e39a6aa4e376fc79dfa96b347bb058
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-08-02 16:54:54 +0900

    #spec #galleryの実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
-  $('.header__menu-icon').click(function() {
+$(document).ready(function () {
+  $('.header__menu-icon').click(function () {
     $('.header__menu').toggleClass('open');
+  });
+
+  $('.gallery__slick').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    dotsClass: "gallery__dots",
+    arrows: false
   });
 });
