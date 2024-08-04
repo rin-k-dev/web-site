d69f51213fad8070e24409b43a8b9c9b472b5344
rink <77056883+rin-k-dev@users.noreply.github.com>
2024-08-04 16:24:03 +0900

    アコーディオンの実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,4 +11,19 @@ $(document).ready(function () {
     dotsClass: "gallery__dots",
     arrows: false
   });
+
+  $(".faq-list__accordion-header").click(function () {
+    const content = $(this).next(".faq-list__accordion-content");
+    const icon = $(this).find(".faq-list__accordion-header-icon i");
+    console.log("Icon before toggle:", icon);
+    content.slideToggle();
+
+    if (content.is(":visible")) {
+      console.log("visible");
+      icon.removeClass("bi-plus").addClass("bi-dash");
+    } else {
+      console.log("invisible");
+      icon.removeClass("bi-dash").addClass("bi-plus");
+    }
+  });
 });
